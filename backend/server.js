371de8990b7fda437ea5691a61a6532e62070a2e
lachezaruzunov <uzunov_89_lacho@abv.bg>
2023-02-07 04:57:45 +0200

    error middleware setup

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
 const PORT = process.env.PORT || 5000;
 const app = express();
+
+// Allows to send raw json, body parser
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.send("Hello");
@@ -9,5 +15,7 @@ app.get("/", (req, res) => {
 
 // Routes
 app.use("/api/users", require("./routes/userRoutes"));
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
